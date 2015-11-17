58394b219f2051dad47d816e1ec9aefb8f84d06b
Tiago Simão <tgsimao@impresa.pt>
2015-11-17 17:08:02 +0000

    removed gelf

--- src/main/java/org/irenical/slf4j/LoggerConfigurator.java
+++ src/main/java/org/irenical/slf4j/LoggerConfigurator.java
@@ -1,8 +1,5 @@
 package org.irenical.slf4j;
 
-import ch.qos.logback.classic.jul.LevelChangePropagator;
-import gelf4j.logback.GelfAppender;
-
 import org.irenical.jindy.Config;
 import org.irenical.jindy.ConfigFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
@@ -11,6 +8,7 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.jul.LevelChangePropagator;
 import ch.qos.logback.classic.spi.Configurator;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.ConsoleAppender;
@@ -21,7 +19,6 @@ import ch.qos.logback.core.spi.ContextAwareBase;
 public class LoggerConfigurator extends ContextAwareBase implements Configurator {
 
   private static final String APPENDER_CONSOLE = "CONSOLE";
-  private static final String APPENDER_GELF = "GELF";
   private static final String APPENDER_FILE = "FILE";
 
   private static final String LEVEL = "log.level";
@@ -34,10 +31,6 @@ public class LoggerConfigurator extends ContextAwareBase implements Configurator
   private static final String FILE_BACKUP_DATE_PATTERN = "log.file.backupdatepattern";
   private static final String FILE_PATH = "log.file.path";
   private static final String FILE_MAXBACKUPS = "log.file.maxbackups";
-
-  private static final String GELF_ENABLED = "log.gelf.enabled";
-  private static final String GELF_HOST = "log.gelf.host";
-  private static final String GELF_PORT = "log.gelf.port";
 
   private static final String DEFAULT_PATTERN = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n";
   private static final String DEFAULT_FILE_PATH = "./log/";
@@ -54,21 +47,22 @@ public class LoggerConfigurator extends ContextAwareBase implements Configurator
 
   public LoggerConfigurator() {
   }
+  
+  protected void initListeners(){
+    CONFIG.listen(LEVEL, this::updateLevel);
+    CONFIG.listen(CONSOLE_ENABLED, this::updateConsole);
+    CONFIG.listen(CONSOLE_PATTERN, this::updateConsole);
+    CONFIG.listen(FILE_ENABLED, this::updateFile);
+    CONFIG.listen(FILE_PATTERN, this::updateFile);
+    CONFIG.listen(FILE_MAXBACKUPS, this::updateFile);
+    CONFIG.listen(FILE_PATH, this::updateFile);
+    CONFIG.listen(FILE_BACKUP_DATE_PATTERN, this::updateFile);
+  }
 
   @Override
   public void configure(LoggerContext loggerContext) {
     if (!started) {
-      CONFIG.listen(LEVEL, this::updateLevel);
-      CONFIG.listen(CONSOLE_ENABLED, this::updateConsole);
-      CONFIG.listen(CONSOLE_PATTERN, this::updateConsole);
-      CONFIG.listen(GELF_ENABLED, this::updateGelf);
-      CONFIG.listen(GELF_HOST, this::updateGelf);
-      CONFIG.listen(GELF_PORT, this::updateGelf);
-      CONFIG.listen(FILE_ENABLED, this::updateFile);
-      CONFIG.listen(FILE_PATTERN, this::updateFile);
-      CONFIG.listen(FILE_MAXBACKUPS, this::updateFile);
-      CONFIG.listen(FILE_PATH, this::updateFile);
-      CONFIG.listen(FILE_BACKUP_DATE_PATTERN, this::updateFile);
+      initListeners();
       started = true;
     }
     loggerContext.reset();
@@ -76,7 +70,6 @@ public class LoggerConfigurator extends ContextAwareBase implements Configurator
     updateLevel();
     updateConsole();
     updateFile();
-    updateGelf();
   }
 
   private void installJulBridge() {
@@ -133,33 +126,6 @@ public class LoggerConfigurator extends ContextAwareBase implements Configurator
 
       } else {
         logbackLogger.detachAppender(fileAppender);
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void updateGelf() {
-    try {
-      LoggerContext loggerContext = (LoggerContext) getContext();
-      Logger logbackLogger = loggerContext.getLogger(Logger.ROOT_LOGGER_NAME);
-
-      GelfAppender<ILoggingEvent> gelfAppender = (GelfAppender<ILoggingEvent>) logbackLogger.getAppender(APPENDER_GELF);
-
-      if (CONFIG.getBoolean(GELF_ENABLED, false)) {
-        logbackLogger.detachAppender(gelfAppender);
-        gelfAppender = new GelfAppender<>();
-        gelfAppender.setHost(CONFIG.getMandatoryString(GELF_HOST));
-        gelfAppender.setPort(CONFIG.getMandatoryInt(GELF_PORT));
-        gelfAppender.setCompressedChunking(true);
-        gelfAppender.setDefaultFields("{\"environment\": \"" + CONFIG.getString("environment") + "\", \"cluster\": \"" + CONFIG.getString("cluster") + "\", \"facility\": \"" + CONFIG.getString("facility") + "\", \"application\": \"" + CONFIG.getString("application") + "\"}");
-        gelfAppender.setAdditionalFields("{\"level\": \"level\", \"logger\": \"loggerName\", \"thread_name\": \"threadName\", \"exception\": \"exception\", \"time_stamp\": \"timestampMs\"}");
-        gelfAppender.setName(APPENDER_GELF);
-        gelfAppender.setContext(loggerContext);
-        gelfAppender.start();
-        logbackLogger.addAppender(gelfAppender);
-      } else {
-        logbackLogger.detachAppender(gelfAppender);
       }
     } catch (Exception e) {
       e.printStackTrace();
